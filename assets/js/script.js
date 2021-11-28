033b84948a8ba1c3ffa29cd7ff01d8f9b8227ef6
toshetasevski <toshetasevski@Toshes-MacBook-Pro.local>
2021-11-28 13:55:51 -0500

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ var quiz = [
         correct: 'answer 1 of 1'
     },
     {
-        question: 'Arraysin JavaScript can be used to store ____',
+        question: 'Arrays in JavaScript can be used to store ____',
         options: ['1. Numbers and Strings', '2. Other Arrays', '3. Booleans', '4. All of the above'],
         correct: 'answer 2 of 2'
     },
@@ -33,25 +33,25 @@ var question = document.querySelector('.question');
 var answers = document.querySelector('.answers');
 var form = document.querySelector('.user');
 var time = document.querySelector('.time');
-var finish = document.querySelector('.finish')
+var finish = document.querySelector('.finish');
 
 startBtn.addEventListener('click', startQuiz);
 
 // created a function to start the game 
 function startQuiz() {
-    startingScreen.setAttribute('class', 'hidden')
-    startTimer()
-    renderQuestionOne()
+    startingScreen.setAttribute('class', 'hidden');
+    startTimer();
+    renderQuestionOne();
 }
 // created timer for the quiz
 function startTimer() {
-    time.textContent = timer
+    time.textContent = timer;
     var setTimer = setInterval(function () {
-        timer--
-        time.textContent = timer
+        timer-- ;
+        time.textContent = timer;
         if (timer === 0) {
-            clearInterval(setTimer)
-            time.textContent = "Time is up !"
+            clearInterval(setTimer);
+            time.textContent = "Time is up !";
         }
     }, 1000)
 }
@@ -59,13 +59,13 @@ function startTimer() {
 
 // this function is rendering the first question 
 function renderQuestionOne() {
-    question.textContent = quiz[0].question
+    question.textContent = quiz[0].question;
 
     for (var i = 0; i < quiz[0].options.length; i++) {
-        var li = document.createElement('li')
-        li.setAttribute('id', quiz[0].options[i])
-        li.textContent = quiz[0].options[i]
-        answers.append(li)
+        var li = document.createElement('li');
+        li.setAttribute('id', quiz[0].options[i]);
+        li.textContent = quiz[0].options[i];
+        answers.append(li);
 
         li.addEventListener('click', function (event) {
             if (event.target.id === quiz[0].correct) {
@@ -73,85 +73,85 @@ function renderQuestionOne() {
                 score += 25;
             } else {
                 console.log('incorrect answer');
-                timer -= 10
+                timer -= 10;
             }
-            renderQuestionTwo()
+            renderQuestionTwo();
         })
     }
 }
 // this function is rendering the second question 
 function renderQuestionTwo() {
-    question.textContent = ''
-    answers.textContent = ''
+    question.textContent = '';
+    answers.textContent = '';
 
-    question.textContent = quiz[1].question
+    question.textContent = quiz[1].question;
     for (var i = 0; i < quiz[1].options.length; i++) {
-        var li = document.createElement('li')
-        li.setAttribute('id', quiz[1].options[i])
-        li.textContent = quiz[1].options[i]
-        answers.append(li)
+        var li = document.createElement('li');
+        li.setAttribute('id', quiz[1].options[i]);
+        li.textContent = quiz[1].options[i];
+        answers.append(li);
 
         li.addEventListener('click', function (event) {
             if (event.target.id === quiz[1].correct) {
                 console.log('correct answer');
-                score += 25
+                score += 25;
             } else {
                 console.log('incorrect answer');
-                timer -= 10
+                timer -= 10;
             }
-            renderQuestionThree()
+            renderQuestionThree();
         })
     }
 }
 // this function is rendering the third question 
 function renderQuestionThree() {
-    question.textContent = ''
-    answers.textContent = ''
+    question.textContent = '';
+    answers.textContent = '';
 
     question.textContent = quiz[2].question
     for (var i = 0; i < quiz[2].options.length; i++) {
-        var li = document.createElement('li')
-        li.setAttribute('id', quiz[2].options[i])
-        li.textContent = quiz[2].options[i]
+        var li = document.createElement('li');
+        li.setAttribute('id', quiz[2].options[i]);
+        li.textContent = quiz[2].options[i];
         answers.append(li)
 
         li.addEventListener('click', function (event) {
             if (event.target.id === quiz[2].correct) {
                 console.log = ('correct answer');
-                score += 25
+                score += 25;
             
             } else {
                 console.log('incorrect answer');
-                timer -= 10
+                timer -= 10;
             }
-            renderQuestionFour()
+            renderQuestionFour();
         })
     }
 }
 // this function is rendering the fourth question 
 function renderQuestionFour() {
-    question.textContent = ''
-    answers.textContent = ''
+    question.textContent = '';
+    answers.textContent = '';
 
     question.textContent = quiz[3].question
     for (var i = 0; i < quiz[3].options.length; i++) {
-        var li = document.createElement('li')
-        li.setAttribute('id', quiz[3].options[i])
-        li.textContent = quiz[3].options[i]
-        answers.append(li)
+        var li = document.createElement('li');
+        li.setAttribute('id', quiz[3].options[i]);
+        li.textContent = quiz[3].options[i];
+        answers.append(li);
 
         li.addEventListener('click', function (event) {
             if (event.target.id === quiz[3].correct) {
                 console.log('Correct answer!');
-                score += 25
+                score += 25;
             } else {
                 console.log('incorrect answer');
-                timer -= 10
+                timer -= 10;
 
             }
 
             endQuiz()
-            question.textContent = "All done!"
+            question.textContent = "All done!";
         })
     }
 }
@@ -172,13 +172,13 @@ function endQuiz() {
 
         var storage = JSON.parse(localStorage.getItem('highScore'))
         if (storage === null) {
-            storage = []
+            storage = [];
         }
         var currentUser = {
             name: input.value,
             currentScore: score
         }
-        storage.push(currentUser)
+        storage.push(currentUser);
         localStorage.setItem('highScore', JSON.stringify(storage))
     })
 }
